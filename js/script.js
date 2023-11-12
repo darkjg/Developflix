e73654a51d299bb88ff350df653be84563ca3665
Jorge <newdarkjg@gmail.com>
2023-11-12 18:34:04 +0100

    Arrays creados

--- js/script.js
+++ js/script.js
@@ -1,2 +1,22 @@
 import peliculas from './peliculas.js'
+const arrayAccion=[];//28
+const arrayThriller=[];//53
+const arrayAventura=[];//12
 
+for (let i = 0; i < peliculas.length; i++) {
+    
+    if(peliculas[i].genre_ids.includes(28)){
+        arrayAccion.unshift(peliculas[i]);
+    }
+    if(peliculas[i].genre_ids.includes(53)){
+        arrayThriller.unshift(peliculas[i]);
+    }
+    if(peliculas[i].genre_ids.includes(12)){
+        arrayAventura.unshift(peliculas[i]);
+    }
+}
+
+console.log(peliculas)
+console.log(arrayAccion);
+console.log(arrayThriller);
+console.log(arrayAventura);
