78024d853e5a19389c684df2f31a95ec6420112e
Jorge <newdarkjg@gmail.com>
2023-11-12 20:05:45 +0100

    Ejercicio acabado

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 import peliculas from './peliculas.js'
+const accion= document.getElementById("genero-28");
+const thriller= document.getElementById("genero-53");
+const aventura= document.getElementById("genero-12");
 const arrayAccion=[];//28
 const arrayThriller=[];//53
 const arrayAventura=[];//12
@@ -16,6 +19,17 @@ for (let i = 0; i < peliculas.length; i++) {
     }
 }
 
+
+for (let i = 0; i < arrayAccion.length; i++) {   
+    accion.innerHTML+="<div ><img src=https://image.tmdb.org/t/p/w500"+arrayAccion[i].poster_path+"><p>"+arrayAccion[i].title+"</p></div>"
+   
+}
+for (let i = 0; i < arrayThriller.length; i++) {
+    thriller.innerHTML+="<div ><img src=https://image.tmdb.org/t/p/w500"+arrayThriller[i].poster_path+"><p>"+arrayThriller[i].title+"</p></div>"
+}
+for (let i = 0; i < arrayAventura.length; i++) {
+    aventura.innerHTML+="<div ><img src=https://image.tmdb.org/t/p/w500"+arrayAventura[i].poster_path+"><p>"+arrayAventura[i].title+"</p></div>"
+}
 console.log(peliculas)
 console.log(arrayAccion);
 console.log(arrayThriller);
